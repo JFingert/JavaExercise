db3393c1c85585ac216ecca820c528f7e55587ba
JFingert <joshuafingert@gmail.com>
2014-01-28 18:03:56 -0800

    a simple exercise written in Java

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		int arg1 = 200,
+		arg2 = 300;
+		System.out.println("4 squared = " + 4 * 4);
+		System.out.println("Number of Args = " + args.length);
+		for (int i = 0; i < arg1; i++) {
+			System.out.println(i);
+			System.out.println(arg2);
+
+		}
+
+	}
+
+}
